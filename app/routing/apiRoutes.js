3d13454879e183fbcdb46305fb06ca0a8058796a
Robert H <robertjameshardin@aol.com>
2019-04-22 16:45:49 -0500

    post request working on client side, server receiving and logging data, need to work out algorithm to find the difference now

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,17 +9,24 @@ module.exports = function (app) {
     res.json(breadData);
   });
   //When user submits a post request to server, calculate compatible match, return match as modal, and save their data into breads.js
-  app.post("/api/breads"), function(req, res) {
-    let newScores;
-    newScores.push(req.body.scores);
-    console.log(newScores);
+  app.post("/api/breads", function(req, res) {
+    //Current function to try to caluclate the difference between scores
     function diff(a, b) {
       return Math.abs(a - b);
     };
+    //Capturing the user's submitted scores
+    let newScores = req.body.scores;
+    //Loggin them to make sure they are received
+    console.log(newScores);
+    //Loop through the breadData array already on server
     for (let i = 0; i < breadData.length; i++) {
+      //Assign the scores arrays of each index to a variable
       let currentScores = breadData[i].scores
-      console.log(diff(newScores, currentScores));
+      //Log each array to the console
+      console.log(currentScores);
+      //Attempt to caluclate the difference between each number in user score and existig score
+      //console.log(diff(newScores, currentScores));
     };
     //breadData.push(req.body)
-  };
+  });
 }
