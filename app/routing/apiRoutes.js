910ea4e7ce52ee5eb8ce8cd4bfbbdb7b38c66c85
Robert H <robertjameshardin@aol.com>
2019-04-24 14:38:35 -0500

    modal working, algorithm needs tweaking

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -15,7 +15,7 @@ module.exports = function (app) {
     //Converting the user's array of strings into an array of integers
     let newScores = userScores.map(x => parseInt(x));
     //Logging them to make sure they are received
-    console.log("User Score");
+    console.log("User Score:");
     console.log(newScores);
     //Assigning chosen json to variable to send to user for modal
     let chosenBread;
@@ -24,6 +24,7 @@ module.exports = function (app) {
       //Assign the scores array of each index to a variable
       let currentScores = allBread.scores
       //Log each array to the console
+      console.log("Existing Scores:")
       console.log(currentScores);
       //Assign variables to hold the question score of each existing breadData.scores array and the newly received user.scores array
       let exisBread = 0;
@@ -38,9 +39,9 @@ module.exports = function (app) {
       //Variable that calculates the difference in scores of the user.scores and each breadData.scores
       let totalDifference = Math.abs(exisBread - usrBread);
       allBread.totalDifference = totalDifference;
-      console.log("breadData " + parseInt(exisBread));
-      console.log("UserData " + parseInt(usrBread));
-      console.log("Total Difference " + parseInt(totalDifference));
+      console.log("breadData Score: " + parseInt(exisBread));
+      console.log("UserData Score: " + parseInt(usrBread));
+      console.log("Total Difference: " + parseInt(totalDifference));
       if (allBread.totalDifference <= 3) {
         chosenBread = allBread;
       } else if (allBread.totalDifference <= 5) {
