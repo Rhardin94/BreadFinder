64d5ed0a771d0933dc4d01fb19a23b2001447d64
Robert H <robertjameshardin@aol.com>
2019-04-24 17:12:50 -0500

    same as above but added more breads to match with

--- app/data/breads.js
+++ app/data/breads.js
@@ -31,7 +31,54 @@ const breads = [
             1,
             3
         ]
+    },
+    {
+        name: "Wheat",
+        photo: "https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/recipes/amish_wheat_bread_recipe/300x300_amish_wheat_bread_recipe.jpg?resize=300px:*",
+        scores: [
+            1,
+            4,
+            2,
+            3,
+            1,
+            2,
+            1,
+            1,
+            1,
+            2
+        ]
+    },
+    {
+        name: "Frech Baguette",
+        photo: "https://redstaryeast.com/wp-content/uploads/2018/02/French-Baguette-72-dpi-800w.jpg",
+        scores: [
+            4,
+            3,
+            5,
+            3,
+            1,
+            3,
+            1,
+            2,
+            1,
+            2
+        ]
+    },
+    {
+        name: "Sourdough",
+        photo: "https://cms.splendidtable.org/sites/default/files/styles/w2000/public/7115933903_a5cdff7c77_z.jpg?itok=DiRxD1eP",
+        scores: [
+            5,
+            3,
+            2,
+            5,
+            2,
+            2,
+            1,
+            2,
+            1,
+            2
+        ]
     }
-
 ]
 module.exports = breads;
